f70c9c04ed8a4ec7148b30e13892a612476e340d
Jun Zhao <jun.zhao@intel.com>
2017-08-01 04:16:30 -0400

    lavc/vaapi_encode: Add max slices number query.
    Add max slices number query.
    
    Signed-off-by: Jun Zhao <jun.zhao@intel.com>

--- libavcodec/vaapi_encode.c
+++ libavcodec/vaapi_encode.c
@@ -971,6 +971,7 @@ static av_cold int vaapi_encode_config_attributes(AVCodecContext *avctx)
         { VAConfigAttribRTFormat         },
         { VAConfigAttribRateControl      },
         { VAConfigAttribEncMaxRefFrames  },
+        { VAConfigAttribEncMaxSlices     },
         { VAConfigAttribEncPackedHeaders },
     };
 
@@ -1103,6 +1104,9 @@ static av_cold int vaapi_encode_config_attributes(AVCodecContext *avctx)
             }
         }
         break;
+        case VAConfigAttribEncMaxSlices:
+            ctx->max_slices = attr[i].value;
+            break;
         case VAConfigAttribEncPackedHeaders:
             if (ctx->va_packed_headers & ~attr[i].value) {
                 // This isn't fatal, but packed headers are always
