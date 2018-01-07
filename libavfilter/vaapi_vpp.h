9928d7cc069b9334819b5be698ee91e1373ac8a9
Jun Zhao <jun.zhao@intel.com>
2018-01-07 21:08:50 +0800

    Add copyleft info
    Signed-off-by: Jun Zhao <jun.zhao@intel.com>

--- libavfilter/vaapi_vpp.h
+++ libavfilter/vaapi_vpp.h
@@ -1,3 +1,24 @@
+/*
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef AVFILTER_VAAPI_VPP_H
+#define AVFILTER_VAAPI_VPP_H
+
 typedef struct VAAPIVPPContext {
     AVVAAPIDeviceContext *hwctx;
     AVBufferRef *device_ref;
@@ -55,4 +76,4 @@ int vaapi_vpp_render_picture(VAAPIVPPContext *ctx, VAProcPipelineParameterBuffer
 //int vaapi_vpp_init(AVFilterContext *avctx);
 
 //void vaapi_vpp_uninit(AVFilterContext *avctx);
-
+#endif /* AVFILTER_VAAPI_VPP_H */
