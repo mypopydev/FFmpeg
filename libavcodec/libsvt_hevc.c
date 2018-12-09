f28ad496f199184fcb494bed48ca2e8bc8ed1733
Jun Zhao <mypopydev@gmail.com>
2018-12-09 22:28:12 +0800

    lavc/libsvt_hevc: fix make fate-source fail
    Missing the "This file is part of FFmpeg." will lead to
    fate-source fail, add this part.
    
    Signed-off-by: Jun Zhao <mypopydev@gmail.com>

--- libavcodec/libsvt_hevc.c
+++ libavcodec/libsvt_hevc.c
@@ -3,12 +3,14 @@
 *
 * Copyright (c) 2018 Intel Corporation
 *
-* This program is free software; you can redistribute it and/or
+* This file is part of FFmpeg.
+*
+* FFmpeg is free software; you can redistribute it and/or
 * modify it under the terms of the GNU Lesser General Public
 * License as published by the Free Software Foundation; either
 * version 2.1 of the License, or (at your option) any later version.
 *
-* This program is distributed in the hope that it will be useful,
+* FFmpeg is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 * Lesser General Public License for more details.
