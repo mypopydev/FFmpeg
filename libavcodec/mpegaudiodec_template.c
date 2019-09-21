2811e25be3f2682af4c2936ac4cf40da671ca5f8
Jun Zhao <barryjzhao@tencent.com>
2019-09-21 23:14:11 +0800

    lavc/mpegaudiodec_template: use the new avpriv_mpeg4audio_get_config
    use the new avpriv_mpeg4audio_get_config.
    
    Signed-off-by: Jun Zhao <barryjzhao@tencent.com>

--- libavcodec/mpegaudiodec_template.c
+++ libavcodec/mpegaudiodec_template.c
@@ -1852,7 +1852,7 @@ static av_cold int decode_init_mp3on4(AVCodecContext * avctx)
     }
 
     avpriv_mpeg4audio_get_config(&cfg, avctx->extradata,
-                                 avctx->extradata_size * 8, 1);
+                                 avctx->extradata_size * 8, 1, avctx);
     if (!cfg.chan_config || cfg.chan_config > 7) {
         av_log(avctx, AV_LOG_ERROR, "Invalid channel config number.\n");
         return AVERROR_INVALIDDATA;
